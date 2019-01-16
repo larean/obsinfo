ffec9881bb389c095c8ca1ad3f7b6355fc5e4d92
Wayne Crawford <crawford@ipgp.fr>
2019-01-16 11:30:15 +0100

    Update distrib.sh
    Removed upload to Test PyPi

--- distrib.sh
+++ distrib.sh
@@ -3,5 +3,5 @@ rm dist/*
 python setup.py sdist
 python setup.py bdist_wheel
 twine check dist/*
-twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
 twine upload dist/*

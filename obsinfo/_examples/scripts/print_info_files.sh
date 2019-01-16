4116018cc4ba62bbf6443b8f0e34598b49ea207d
Wayne Crawford <crawford@ipgp.fr>
2019-01-16 11:40:57 +0100

    Update print_info_files.sh

--- obsinfo/_examples/scripts/print_info_files.sh
+++ obsinfo/_examples/scripts/print_info_files.sh
@@ -2,20 +2,12 @@
 INSTRUMENTATION_DIR="../Information_Files/instrumentation/INSU-IPGP.2018-06-01"
 CAMPAIGN_DIR="../Information_Files/campaigns/MYCAMPAIGN"
 
-echo "=============================================================="
-echo "== FILES IN CAMPAIGN DIRECTORY: $CAMPAIGN_DIR"
-echo "=============================================================="
+echo "====== FILES IN CAMPAIGN DIRECTORY: $CAMPAIGN_DIR ======"
 for f in ${CAMPAIGN_DIR}/*.yaml; do
-    echo "___________________________________________________________"
     obsinfo-print "$f"
-    echo "-----------------------------------------------------------"
-    echo "|"; echo "|"; echo "|"
 done
-echo ""
-echo "=============================================================="
-echo "== FILES IN INSTRUMENTATION DIRECTORY: $INSTRUMENTATION_DIR"
-echo "=============================================================="
+
+echo "== FILES IN INSTRUMENTATION DIRECTORY: $INSTRUMENTATION_DIR =="
 for f in ${INSTRUMENTATION_DIR}/*.yaml; do
-    echo "___________________________________________________________"
     obsinfo-print "$f"
 done

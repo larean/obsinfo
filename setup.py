86bad3fb652024c826c21ccd6e62b4778cc6f84a
Wayne Crawford <crawford@ipgp.fr>
2019-03-28 16:38:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setuptools.setup(
             'obsinfo-validate=obsinfo.misc.info_files:_validate_script',
             'obsinfo-print=obsinfo.misc.print:_print_script',
             'obsinfo-makeSTATIONXML=obsinfo.network.network:_make_stationXML_script',
-            'obsinfo-SDPCHAIN_scripts=obsinfo.addons.SDPCHAIN:_console_script',
-            'obsinfo-LCHEAPO_scripts=obsinfo.addons.LCHEAPO:_console_script'
+            'obsinfo-make_SDPCHAIN_scripts=obsinfo.addons.SDPCHAIN:_console_script',
+            'obsinfo-make_LCHEAPO_scripts=obsinfo.addons.LCHEAPO:_console_script'
         ]
     },
     python_requires='>=3',
